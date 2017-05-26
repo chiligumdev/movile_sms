6d5b415140da505bd5c0819dd1d49f515e26a783
leandrochili <lndr.figueredo@gmail.com>
2017-05-26 15:31:49 -0300

    Adding basic dependencies

--- lib/movile_sms.rb
+++ lib/movile_sms.rb
@@ -1,5 +1,24 @@
 require "movile_sms/version"
+require "movile_sms/sms"
+
+require 'faraday'
+require 'json'
+
+API_URL = "https://api-messaging.movile.com/v1/send-sms"
 
 module MovileSms
-  # Your code goes here...
+
+  class Sms
+
+    attr_reader :username, :access_token
+
+    def initialize(attributes)
+      @username     = attributes["username"]
+      @access_token = attributes["access_token"]
+    end
+
+    def self.get_user
+    end
+  end
+
 end
--- movile_sms.gemspec
+++ movile_sms.gemspec
@@ -31,4 +31,10 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "minitest"
+  spec.add_development_dependency "vcr"
+  spec.add_development_dependency "webmock"
+
+  spec.add_dependency "faraday"
+  spec.add_dependency "json"
 end
