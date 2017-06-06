4fd1019d5ae8ef292ce7cdd4f1fbbc1ca8fe94c6
leandrochili <lndr.figueredo@gmail.com>
2017-06-06 15:33:05 -0300

    New Endpoint Status Message

--- lib/movile_sms/sms.rb
+++ lib/movile_sms/sms.rb
@@ -4,6 +4,8 @@ class SMS
 
   BASE_API_URL = 'https://api-messaging.movile.com/v1/send-sms'.freeze
 
+  REPORT_API_URL = 'https://api-messaging.movile.com/v1/sms-status?id='.freeze
+
   def initialize(attributes)
     @options = {}
     @options['UserName'] = attributes[:username]
@@ -14,7 +16,13 @@ class SMS
   def send_message(number, text)
     raise 'Not Valid' unless numeric?(number) && valid_text?(text)
     body = { 'destination' => number, 'messageText' => text }.to_json
-    self.class.post(BASE_API_URL, headers: @options, body: body)
+    response = self.class.post(BASE_API_URL, headers: @options, body: body)
+    response['id']
+  end
+
+  def status_message(uuid)
+    response = self.class.get(REPORT_API_URL + uuid, headers: @options)
+    JSON.parse(response.body)
   end
 
   # Return false if the value isn't a numeric value
